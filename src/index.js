d65a22293a46934e9683221a5eaece0faac204fa
magujun <guimaraesjunior.marcelo@gmail.com>
2021-03-13 18:58:12 +0100

    chore: finished

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
 const users = [];
-// const todoUpdate = [];
 
 app.use(cors());
 app.use(express.json());
@@ -16,12 +15,12 @@ app.use(express.json());
  * todos = []
  */
 
-// Middleware check user & todos
-function checkIfUserExists(request, response, next) {
+// Middleware check user
+function checkExistsUserAccount(request, response, next) {
 	const { username } = request.headers;
-	const user = users.find((user) => user.username === username);
-	if (!user) {
-		return response.status(400).json({ error: 'Username not found!' });
+	const user = users.find(user => user.username === username);
+	if(!user) {
+		return response.status(404).json({ error: 'Username not found!' });
 	}
 	request.user = user;
 	return next();
@@ -30,92 +29,98 @@ function checkIfUserExists(request, response, next) {
 app.post('/users', (request, response) => {
 	const { username, name } = request.body;
 	const userAlreadyExists = users.some((user) => user.username === username);
-	if (userAlreadyExists) {
+	if(userAlreadyExists) {
 		return response.status(400).json({ error: 'Username already exists!' });
 	}
-	users.push({
+	const user = {
 		id: uuidv4(),
-		username,
 		name,
+		username,
 		todos: []
-	});
-	return response.status(201).json({ message: 'User ' + name + ' succesfully created with username ' + username });
+	};
+	users.push(user);
+	return response.status(201).json(user);
+	// return response.status(201).json({ message: 'User ' + name + ' succesfully created with username ' + username });
 });
 
 // Use middleware check for all routes
-app.use(checkIfUserExists);
+// app.use(checkExistsUserAccount);
 
-app.post('/todos', (request, response) => {
-	const { title, description, deadline } = request.body;
+app.get('/todos', checkExistsUserAccount, (request, response) => {
 	const { user } = request;
 	const username = user.username;
-	const todoTask = {
-		title,
-		description,
-		deadline: new Date(deadline),
-		id: uuidv4(),
-		created_at: new Date(),
-		done: false
-	};
-	user.todos.push(todoTask);
-	return response.status(201).json({ message: 'Task ' + title + ' created for user ' + username + ' with deadline ' + deadline + '!' }).send();
-});
-
-app.get('/todos', (request, response) => {
-	const { user } = request;
-	const username = user.username;
-	if (user.todos == []) {
+	// const title = user.title;
+	if(user.todos == []) {
 		return response.status(400).json({ error: 'No toDos found for user ' + username + '!' });
 	} else {
-		return response.json((user.todos));
+		return response.json(user.todos);
+		// return response.json({message: 'Task ' + title + ' created for user ' + username + ' with deadline ' + deadline});
 	}
 });
 
-app.put('/todos/:id', (request, response) => {
+app.post('/todos', checkExistsUserAccount, (request, response) => {
+	const { user } = request;
+	const { title, deadline } = request.body;
+	// const username = user.username;
+	const todoTask = {
+		id: uuidv4(),
+		title,
+		done: false,
+		deadline: new Date(deadline),
+		created_at: new Date() 
+	};
+	user.todos.push(todoTask);
+	// return response.status(201).json({ message: 'Task ' + title + ' created for user ' + username + ' with deadline ' + deadline + '!' }).send();
+	return response.status(201).json(todoTask);
+});
+
+app.put('/todos/:id', checkExistsUserAccount, (request, response) => {
 	const { user } = request;
 	const { id } = request.params;
 	const todoUpdate = request.body;
 	const todo = user.todos.find(todo => todo.id === id);
-	if (!todo) {
+	if(!todo) {
 		return response.status(404).json({ error: 'Task not found!' });
 	}
 	todo.title = todoUpdate.title;
-	todo.description = todoUpdate.description;
 	todo.deadline = new Date(todoUpdate.deadline);
-	return response.json({message: 'Task "' + todo.title + '" updated!'}).send();
+	// return response.json({message: 'Task "' + todo.title + '" updated!'}).send();
+	return response.json(todo);
 });
 
-app.patch('/todos/:id', (request, response) => {
+app.patch('/todos/:id/done', checkExistsUserAccount, (request, response) => {
 	const { user } = request;
 	const { id } = request.params;
-	const status = request.query;
 	const todo = user.todos.find(todo => todo.id === id);
-	if (!todo) {
+	if(!todo) {
 		return response.status(404).json({ error: 'Task not found!' });
 	}
-	const done = status.done ? (status.done.toLowerCase() == 'true') : false;
-	if (done === true && todo.done === done) {
-		return response.status(404).json({ error: 'Task already completed!'});
-	}
-	if (done === false && todo.done === done) {
-		return response.status(404).json({ error: 'Task still pending!' });
-	}
+	// const status = request.query;
+	// const done = status.done ? (status.done.toLowerCase() == 'true') : false;
+	// if(done === true && todo.done === done) {
+	// 	return response.status(404).json({ error: 'Task already completed!'});
+	// }
+	// if(done === false && todo.done === done) {
+	// 	return response.status(404).json({ error: 'Task still pending!' });
+	// }
+	const done = true;
 	todo.done = done;
-	const state = 'task pending!';
-	if(done === true) { const state = 'task completed!'; }
-	return response.json({ message: todo.title + ' status updated: ' + state }).send();
+	// const state = 'task pending!';
+	// if(done === true) { const state = 'task completed!'; }
+	// return response.json({ message: todo.title + ' status updated: ' + state }).send();
+	return response.json(todo);
 });
 
-app.delete('/todos/:id', (request, response) => {
+app.delete('/todos/:id', checkExistsUserAccount, (request, response) => {
 	const { user } = request;
 	const { id } = request.params;
-	const todo = user.todos.find(todo => todo.id === id);
 	const todoIndex = user.todos.findIndex(todo => todo.id === id);
-	if (todoIndex === -1) {
+	if(todoIndex === -1) {
 		return response.status(404).json({ error: 'Task not found!' });
 	}
 	user.todos.splice(todoIndex, 1);
-	return response.status(204).json({ message: 'Task "' + todo.title + '" deleted!' }).send();
+	// return response.status(204).json({ message: 'Task "' + todo.title + '" deleted!' }).send();
+	return response.status(204).json();
 });
 
 module.exports = app;
